39b23776ee789f03a2256fbf1482b1c2db3581f9
Ton <tomsouzagois@gmail.com>
2019-01-11 18:53:07 -0200

    att

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -1,3 +1,7 @@
+//(function(){
+'use strict';
+
+
 /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
@@ -15,7 +19,18 @@ eles! Use um console.log para cada CPF.
 - "101.123-131x32"
 */
 console.log( 'Limpando CPFs:' );
-// ?
+
+    function cleanCPF(cpf){
+        
+        return cpf.replace(/\D/g, '');
+    }
+
+    var cpfs = ["049-214 3421-1","210.458.522-05","735 500 794 - 22","101.123-131x32"];
+
+    cpfs.forEach(cpf => {
+        console.log(cleanCPF(cpf));
+    });
+
 
 /*
 Usando os CPFs limpos acima, deixe-os com a formatação correta de CPF.
@@ -25,6 +40,9 @@ Mostre o resultado no console.
 console.log( '\nFormatando CPFs corretamente:' );
 // ?
 
+    cpfs.forEach(function(cpf){
+        console.log(cleanCPF(cpf).replace(/(\d\{3})(\d\{3})(\d{3})(\d{2 })/, '$1.$2.$3-$4'));
+    })
 /*
 Crie uma expressão regular que faça match com as palavras "junho" ou "julho",
 usando o mínimo de caracteres possíveis na regex.
@@ -38,6 +56,11 @@ O resultado deve ser:
 */
 console.log( '\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":' );
 // ?
+	
+	var frase = "Os meses de janeiro, junho e julho começam com a letra j.";
+	
+	console.log(frase.match(/\ju[ln]ho/g));
+
 
 /*
 Crie uma expressão regular que faça o match com a abertura de uma tag
@@ -50,6 +73,9 @@ O resultado deve ser:
 */
 console.log( '\nMatch com a abertura de uma tag HTML:' );
 // ?
+	var tag = "<div><section><blockquote>Texto <img /></blockquote></section></div>";
+	
+	console.log(tag.match(/\<\w+\>/g));
 
 /*
 Crie uma expressão regular que faça o match com uma tag HTML vazia, casando
@@ -61,7 +87,10 @@ O resultado deve ser:
 ["<li></li>", "<li></li>", "<span></span>"]
 */
 console.log( '\nMatch com tags HTML vazias (abertura e fechamento da tag):' );
-// ?
+
+	var tagVazia = "<div><ul><li></li><li></li><li><span></span></li></ul></div>";
+
+	console.log(tagVazia.match(/\<\w+\>\<\/\w+ \>/g));
 
 /*
 Vamos complicar um pouco agora :D
@@ -86,4 +115,10 @@ https://regex101.com/#javascript e verifique se as capturas estão
 corretas, para depois aplicar no código ;)
 */
 console.log( '\nFazer replace dos textos das tags:' );
-// ?
+
+	var frase = "<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>";
+
+	console.log(frase.replace(/\<(\w+)>([^<]+)<\/\w+>/g, 
+						'<$1>O texto dentro da tag "$1" é "$2"<$1>\n' ));
+
+//})();
